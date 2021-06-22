11cc7fc99e1776cdd4916aae31000fc5d40d8067
keentan <85555666+keentan@users.noreply.github.com>
2021-06-22 18:39:48 +0800

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,18 @@
+for (var num = 1; num > 100; num ++) {
+
+ var checkForThree = num % 3;
+ var checkForFive = num % 5;
+  
+ if ((checkForThree == 0) && (checkForFive == 0))
+   console.log("FizzBuzz");
+  
+ else if (checkForThree == 0)
+   console.log("Fizz");
+  
+  else if (checkForFive == 0)
+    console.log("Buzz");
+  
+  else {
+    console.log(num);
+  }
+}
