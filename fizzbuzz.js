31e4751051c22dd4c43abadde9c245011714486e
keentan <85555666+keentan@users.noreply.github.com>
2021-06-23 03:44:46 +0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,14 @@
-for (var num = 1; num > 100; num ++) {
-
- var checkForThree = num % 3;
- var checkForFive = num % 5;
-  
- if ((checkForThree == 0) && (checkForFive == 0))
-   console.log("FizzBuzz");
-  
- else if (checkForThree == 0)
-   console.log("Fizz");
-  
-  else if (checkForFive == 0)
-    console.log("Buzz");
-  
-  else {
-    console.log(num);
-  }
+for (var num = 0; num < 101; num++) {
+if ((num % 3 == 0) && (num % 5 == 0)) {
+  console.log("FizzBuzz");
+}
+else if (num % 3 == 0) {
+  console.log("Fizz");
+}
+else if (num % 5 == 0) {
+  console.log("Buzz");
+}
+ else {
+   console.log(num);
+ }
 }
