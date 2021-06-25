e862dd572b5d482abe169a712a5c4f6d354a521f
keentan <85555666+keentan@users.noreply.github.com>
2021-06-25 17:48:13 +0800

    Update day2.js

--- day2.js
+++ day2.js
@@ -1,39 +1,40 @@
-// Declare a function - to group it
-
-function sayHello(){
+// This is how you declare a function, instead of doing "console.log 3 times", we can declare a function so that we wont have to repeat "console.log"
+function sayHello() {
   console.log("Hello");
 }
-// Call or invoke a function
+// Need to call or invoke a function then only it will appear in the console
 
 sayHello();
-sayHello();
-sayHello();
 
-// Declaring a function with variable / parameter
+// Delcaring a function with variable / parameter (over time we need to use this vocab instead)
 
 function sayGoodbye(name) {
   console.log("Goodbye "+name);
 }
+
 sayGoodbye("Ash");
 sayGoodbye("Raj");
 sayGoodbye("Syamil");
 
 // Function with 2 parameters with result
 function sum(a,b) {
-  return a+b;
+  return a+b
 }
-console.log(sum(2,3));
-console.log(sum(4,8));
+console.log(sum(10,4));
 
-// Function with result..(Example of abstarction.. if you need to use it later, you can use)
-function checkEven(number) {
-  if (number % 2 == 0) {
+// Function with result (example of abstraction if you need to use it later, no need to declare a variable beforehand, just put the number in the bracket)
+function checkEven (num) {
+  if (num % 2 == 0) {
     return true;
   }
   else {
     return false;
   }
 }
-console.log(checkEven(3));
+console.log(checkEven(10));
 
-// Function with multiple results..
+// Function with multiple results
+function areaVolume(width, length, height) {
+  return [width * length * height, width * length];
+}
+console.log(areaVolume(3,4,5))
