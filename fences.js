1f43e6be411e98dadcd06e9b9dce902b334bf452
keentan <85555666+keentan@users.noreply.github.com>
2021-07-30 18:03:25 +0800

    Update fences.js

--- fences.js
+++ fences.js
@@ -2,7 +2,7 @@ var lines = 9
 
 for (var j = 0; j < lines; j++) {
 for (var i=0; i < lines; i++) {
-  if ((i % 2 == 0 && j % 2 == 0)) || (i % 2 == 1 && j % 2 == 1)) {
+  if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
     document.write("#");
   }
   else {
